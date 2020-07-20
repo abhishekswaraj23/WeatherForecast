fa92d5683d717c9c45d1cc08154f321e8f35448c
Abhishek Swaraj <abhishekswaraj@Abhisheks-MacBook-Pro.local>
2020-07-20 10:48:23 +0530

    App icon android

--- WeatherForecast.Android/Resources/Resource.designer.cs
+++ WeatherForecast.Android/Resources/Resource.designer.cs
@@ -6758,6 +6758,9 @@ namespace WeatherForecast.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int weathericon = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
